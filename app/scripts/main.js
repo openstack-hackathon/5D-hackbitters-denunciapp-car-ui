0c9245b1d44097555b67cfb584d0aed15d5243f8
Edgar Baeza <ebaeza1992@gmail.com>
2016-09-11 06:44:25 -0500

    icono de visualización en lista de denuncias

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,37 @@
 import * as config from './config.js'
 
+function getComplaintDetail(e) {
+  e.stopPropagation()
+  let id = $(this).attr('data-id')
+  $.get(`${config.API_BASE}complaint/${id}`, function(data) {
+    console.log(data)
+  })
+  return false
+}
+
 $(function () {
-  $.get(config.API_BASE + 'complaint', function( data ) {
+  // Get the last complaints
+  $.get(config.API_BASE + 'complaint', function(data) {
     let $listContainer = $('#list-complaints')
 
-    data.forEach((complaint) => {
-      console.log(complaint)
-      let date = new Date(Date.parse(complaint.createTime)).toLocaleString()
+    $listContainer.find('#load-list-complaints').fadeOut(400, ()=> {
+      // render last complaints
+      data.forEach((complaint) => {
+        let date = new Date(Date.parse(complaint.createTime)).toLocaleString()
 
-      // append last complaints
-      $listContainer.append(`<a href="" class="collection-item" data-id="${complaint._id}">
+        $listContainer.append(`<li href="" class="collection-item" >
         <div>Placa: ${complaint.plate}
-          <span class="new badge blue" data-badge-caption="snapshots">${complaint.snapshots.length}</span>
+        <a href="#" class="secondary-content" data-id="${complaint._id}">
+        <i class="material-icons">visibility</i></a>
         </div>
         <div>Fecha: ${date}</div>
-      </a>`)
-
-      $listContainer.find('.collection-item').click((e) => {
-        e.stopPropagation()
-        console.log('Hi!!')
-        return false
+        </li>`)
       })
-    });
-  }, 'json');
+
+      // consult the detial of complaint
+      let $itemsAction = $listContainer.find('.collection-item a')
+      $itemsAction.click(getComplaintDetail)
+    })
+
+  }, 'json')
 })
